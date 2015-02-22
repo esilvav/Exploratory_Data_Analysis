4d32dc3bf74cf8608b5dccc67257fd9225f2bb34
Edgar Omar Silva V <edgar.silvav@gmail.com>
2015-02-22 17:22:09 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -14,11 +14,10 @@ NEI <- readRDS("D:/censo 2010/data/summarySCC_PM25.rds")
 head(NEI)
 SCC <- readRDS("D:/censo 2010/data/Source_Classification_Code.rds")
 head(SCC, n = 5)
-
 years <- c("1999", "2002", "2005", "2008")
 means <- vector()
 for (i in years) {
     means[i] <- mean(NEI$Emissions[which(NEI$year == i)], na.rm = TRUE)
 }
-# basic plot
+png(filename='D:/censo 2010/data/Plot1.png')
 barplot(means, col = rainbow(20, start = 0, end = 1), main = "Mean PM_2.5 emissions (Tons)")
